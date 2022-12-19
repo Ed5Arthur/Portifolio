c8b5258e15836a4c9daf3c4da902fd7b30c79313
unknown <arthurdosreisaraujo@gmail.com>
2022-12-19 09:30:59 -0300

    Responsividade Aplicada

---
+++ Site Game Mania/js/script.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById("menu")
+function mostrarMenu() {
+    if (menu.style.display != "flex") {
+        menu.style.display = "flex"
+    } else {
+        menu.style.display = "none"
+    }
+}
