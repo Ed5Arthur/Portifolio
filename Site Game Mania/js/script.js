fe5351cbddbf37f181fba67141cf4a5b993eed68
unknown <arthurdosreisaraujo@gmail.com>
2023-01-09 13:04:26 -0300

    Jquey

--- Site Game Mania/js/script.js
+++ Site Game Mania/js/script.js
@@ -1,8 +1,20 @@
+$(document).ready(function () {
+
+    $("#barras").click(function () {
+        $("#menu").toggleClass("#menu-ativo")
+    })
+})
+
+
+
+
 let menu = document.getElementById("menu")
-function mostrarMenu() {
-    if (menu.style.display != "flex") {
-        menu.style.display = "flex"
-    } else {
-        menu.style.display = "none"
-    }
-}
+
+
+//function mostrarMenu() {
+  //  if (menu.style.display != "flex") {
+    //    menu.style.display = "flex"
+    //} else {
+      //  menu.style.display = "none"
+    //}
+//}
